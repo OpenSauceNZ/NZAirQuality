e6c0a6e6f334a0a5e930d19929797fd2d8fdb6cd
Liguo Jiao's Mac <jiaoliguo888@gmail.com>
2017-07-27 22:24:29 +1200

    commit for save

--- NZAirQuality/Controller/WeatherViewController.swift
+++ NZAirQuality/Controller/WeatherViewController.swift
@@ -16,13 +16,12 @@ class WeatherViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return 1
     }
     
@@ -34,26 +33,9 @@ class WeatherViewController: UITableViewController {
         }
         
     }
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 230
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+    
 }
