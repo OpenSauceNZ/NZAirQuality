2cab99c21bdfb54501c1756eb5e1790ded6dec22
Liguo Jiao's Mac <jiaoliguo888@gmail.com>
2017-07-28 16:12:57 +1200

    finish auto layout setup

--- NZAirQuality/Controller/WeatherViewController.swift
+++ NZAirQuality/Controller/WeatherViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class WeatherViewController: UITableViewController {
-
+class WeatherViewController: UITableViewController, UISearchResultsUpdating {
+    
+    var searchController: UISearchController!
+    var searchResultController = UITableViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.backgroundColor = NZABackgroundColor
+        setGradientBackground()
     }
-
+    
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 255.0/255.0, green: 149.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [ colorTop, colorBottom]
+        gradientLayer.locations = [ 0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+        
+        self.tableView.backgroundView?.layer.addSublayer(gradientLayer)
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -22,7 +42,7 @@ class WeatherViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- NZAirQuality/View/Weather/WeatherHeaderViewCell.swift
+++ NZAirQuality/View/Weather/WeatherHeaderViewCell.swift
@@ -9,9 +9,35 @@
 import UIKit
 
 class WeatherHeaderViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var weatherStatusImage: UIImageView!
+    @IBOutlet weak var conponentImage: UIImageView!
+    @IBOutlet weak var montainImage: UIImageView!
+    @IBOutlet weak var weatherInfoSection: WeatherInfoView!
+    @IBOutlet weak var tempture: UILabel!
+    
+    @IBOutlet weak var location: UILabel!
+    
+    @IBOutlet weak var weatherStatus: UILabel!
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.selectionStyle = .none
+        self.backgroundColor = NZABackgroundColor
+        self.tempture.text = "25℃"
+        
+        tempture.backgroundColor = UIColor.clear
+        tempture.textColor = UIColor.white
+        
+        tempture.textAlignment = .center
+        tempture.numberOfLines = 1
+        tempture.layer.shadowColor = UIColor.white.cgColor
+        tempture.layer.shadowOffset = CGSize(width: 3, height: 3)
+        tempture.layer.shadowOpacity = 0.8
+        tempture.layer.shadowRadius = 6
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
